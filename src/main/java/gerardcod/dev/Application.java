f14801c1d4c69ac79b6aaaee90584c4dc986b22b
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-08-28 23:23:07 -0500

    modify: Adding printNames method to Application class

--- src/main/java/gerardcod/dev/Application.java
+++ src/main/java/gerardcod/dev/Application.java
@@ -1,6 +1,14 @@
 package gerardcod.dev;
 
+import java.util.List;
+
 public class Application {
+
+    public static void printNames() {
+        List<String> names = List.of("Gerardo", "Roberto", "María", "Fernanda");
+        names.forEach(System.out::println);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello from Application");
     }
